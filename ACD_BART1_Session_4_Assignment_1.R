bdbf120dbc85c6bb145a777d43cf08dc819cf884
ramanmgr <35422787+ramanmgr@users.noreply.github.com>
2018-03-06 13:39:24 +0530

    Add files via upload

---
+++ ACD_BART1_Session_4_Assignment_1.R
@@ -0,0 +1,17 @@
+df1 = data.frame(CustId = c(1:6), Product = c(rep("TV", 3), rep("Radio", 3)))
+df2 = data.frame(CustId = c(2, 4, 6), State = c(rep("Texas", 2), rep("NYC", 1)))
+df1 #left table
+df2 #right table
+#For the above given data frames and tables perform the following operations:
+
+##1. Return only the rows in which the left table have match
+
+m1 <- merge(df1,df2,by="CustId")
+m1
+
+##2. Return all rows from both tables, join records from the left 
+#which have matching keys in the right table.
+
+m2<-merge(df1,df2,all = TRUE)
+m2
+
